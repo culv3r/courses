da2f295be174824b76742185295d81b17407f374
swf474 <swf474@fox04.cs.utsa.edu>
2016-10-19 22:57:06 -0500

    Updating ccat

--- cs/3423/assign5/xccat.c
+++ cs/3423/assign5/xccat.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #define MAX_BUF_SIZE 1024
-
+#define MAX_TOKEN_SIZE 512 
+#define TRUE 1
+#define FALSE 0
 
 int main (int argc, char *argv[]){
     int fd1; //file descriptor
     int fd2; //file descriptor
-    int wfd = -1; //file descriptor for write
+    int wfd; //file descriptor for write
     int bInput1 = 0; //Boolean input flag for whether inputs are Stdin or not.
     int bInput2 = 0;
     char szBuf[MAX_BUF_SIZE];
     char szBuf2[MAX_BUF_SIZE];
     ssize_t size;
     ssize_t size2;
-    int iWrite;
 
     if (argc < 3 || ( argc > 4 && ( strcmp("-",argv[1]) != 0 || strcmp("-",argv[2]) != 0 ))){
         //Arguments are outside of specifications, kill the program and list usage.
@@ -62,8 +60,7 @@ int main (int argc, char *argv[]){
             }
         }
         if (i == 3){
-            wfd = open(argv[3], O_WRONLY | O_CREAT | O_TRUNC | O_SYNC, 0666);
-            printf("Write file opened, %s\n", argv[i]);
+            wfd = open(argv[i], O_WRONLY | O_CREAT | O_APPEND, 0666);
             if (wfd == -1){
                 perror(argv[3]);
                 return 1;
@@ -73,79 +70,54 @@ int main (int argc, char *argv[]){
     }
     //Setup I/O transactions
     if ((fd1 != 0 && fd2 != 0) && argc < 4){
-        while ((size = read(fd1, szBuf, sizeof szBuf)) > 0){
-            if (wfd == -1){
-                write(STDOUT_FILENO,szBuf,size);
+        int x = 0;
+        int y = 0;
+        int bDone = 0;
+        while ((size = read(fd1, szBuf, sizeof szBuf)) > 0 || (size2 = read(fd2, szBuf2, sizeof szBuf2)) > 0){
+            while(szBuf[x] != '\n' && x >= size){
+                write(STDOUT_FILENO,szBuf[x],sizeof(szBuf[x]));
+                x++;
             }
-            if (wfd != -1){
-                iWrite = write(wfd,&szBuf,size);
-                if (iWrite < 0)
-                    return 0;
+            write(STDOUT_FILENO,'\n',sizeof szBuf[x]);
+            x++;
+            while(szBuf2[y] != '\n' && y <= size2){
+                write(STDOUT_FILENO,szBuf2[y],sizeof szBuf[x]);
+                y++;
             }
-        }
-        while ((size2 = read(fd2, szBuf2, sizeof szBuf2)) > 0){
-            if (wfd == -1){
-                write(STDOUT_FILENO,szBuf2,size2);
-            }
-            if (wfd != -1){
-                iWrite = write(wfd,szBuf2,size2);
-                if (iWrite < 0)
-                    return 0;
-            }
+            write(STDOUT_FILENO,'\n',sizeof szBuf[x]);
+            y++
         }
 
     }
     //Stdin for first input, read file for second input.
     if ((bInput1 && fd2 != 0) && argc < 4){
-        while((size = read(STDIN_FILENO, szBuf, sizeof szBuf)) > 0){
-            if (wfd == -1){
-                if (szBuf[0] != '\0')
-                    write(STDOUT_FILENO, szBuf, size);
+        printf("[User Input Required, Please type your lines, when finished press CTRL + D]\n");
+        while((size = read(STDIN_FILENO, szBuf, sizeof szBuf)) > 0 || (size2 = read(fd2, szBuf2, sizeof szBuf2)) > 0){
+            if (szBuf[0] != '\0')
+                write(STDOUT_FILENO, szBuf, size);
+            while(szBuf2[y] != '\n' && y <= size2){
+                write(STDOUT_FILENO, szBuf2[y], 1);
+                y++;
             }
-            if (wfd != -1){
-                if (szBuf[0] != '\0')
-                    iWrite = write(wfd,szBuf,size);
-                if (iWrite < 0)
-                    return 0;
-            }
-        }
-        while ((size2 = read(fd2, szBuf2, sizeof szBuf2)) > 0){
-            if (wfd == -1){
-                write(STDOUT_FILENO,szBuf2,size2);
-            }
-            if (wfd != -1){
-                iWrite = write(wfd,szBuf2,size2);
-                if (iWrite < 0)
-                    return 0;
-            }
+            write(STDOUT_FILENO, '\n', 1);
+            y++;
         }
     }
     
     //Stdin for second input...
     if ((fd1 != 0 && bInput2) && argc < 4){
-        while((size = read(fd1, szBuf, sizeof szBuf)) > 0){
-            if (wfd == -1){
-                write(STDOUT_FILENO,szBuf,size);
+        while((size = read(fd1, szBuf, sizeof szBuf)) > 0 && (size2 = read(STDIN_FILENO, szBuf2, sizeof szBuf2)) > 0){
+            while(szBuf[x] != '\n' && x <= size){
+                write(STDOUT_FILENO, szBuf[x], 1);
+                x++;
             }
-            if (wfd != -1){
-                iWrite = write(wfd,szBuf,size);
-                if (iWrite < 0)
-                    return 0;
-            }
-        }
-        while((size2 = read(STDIN_FILENO, szBuf2, sizeof szBuf2)) > 0){
-            if (wfd == -1){
-                write(STDOUT_FILENO,szBuf2,size2);
-            }
-            if (wfd != -1){
-                iWrite = write(wfd,szBuf2,size2);
-                if (iWrite < 0)
-                    return 0;
-            }
+            write(STDOUT_FILENO, '\n', 0);
+            x++;
+            printf("[User Input Required, Please type your lines, when finished press CTRL + D]\n");
+            if((size2 = read(STDIN_FILENO, szBuf2, sizeof szBuf2)) > 0){
+                write(STDOUT_FILENO, szBuf2, size2);
+            } 
         }
     }
-    close(fd1);
-    close(fd2);
-    close(wfd);
-    return 0;
 }
+
