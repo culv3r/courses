bec6a5d89bd5a95fabf49dfd986ff19b8dfc618b
Daniel Larsen <danny.r.larsen@gmail.com>
2016-10-19 23:23:17 -0500

    ccat final

--- cs/3423/assign5/ccat.c
+++ cs/3423/assign5/ccat.c
@@ -62,7 +62,7 @@ int main (int argc, char *argv[]){
             }
         }
         if (i == 3){
-            wfd = open(argv[3], O_WRONLY | O_CREAT | O_TRUNC | O_SYNC, 0666);
+            wfd = open(argv[3], O_WRONLY | O_CREAT | O_TRUNC, 0666);
             printf("Write file opened, %s\n", argv[i]);
             if (wfd == -1){
                 perror(argv[3]);
@@ -72,7 +72,7 @@ int main (int argc, char *argv[]){
         }
     }
     //Setup I/O transactions
-    if ((fd1 != 0 && fd2 != 0) && argc < 4){
+    if ((fd1 != 0 && fd2 != 0)){
         while ((size = read(fd1, szBuf, sizeof szBuf)) > 0){
             if (wfd == -1){
                 write(STDOUT_FILENO,szBuf,size);
@@ -96,15 +96,15 @@ int main (int argc, char *argv[]){
 
     }
     //Stdin for first input, read file for second input.
-    if ((bInput1 && fd2 != 0) && argc < 4){
+    if ((bInput1 && fd2 != 0)){
         while((size = read(STDIN_FILENO, szBuf, sizeof szBuf)) > 0){
             if (wfd == -1){
                 if (szBuf[0] != '\0')
                     write(STDOUT_FILENO, szBuf, size);
             }
-            if (wfd != -1){
+            else{
                 if (szBuf[0] != '\0')
-                    iWrite = write(wfd,szBuf,size);
+                    iWrite = write(wfd,szBuf,size); 
                 if (iWrite < 0)
                     return 0;
             }
@@ -113,7 +113,7 @@ int main (int argc, char *argv[]){
             if (wfd == -1){
                 write(STDOUT_FILENO,szBuf2,size2);
             }
-            if (wfd != -1){
+            else{
                 iWrite = write(wfd,szBuf2,size2);
                 if (iWrite < 0)
                     return 0;
@@ -122,7 +122,7 @@ int main (int argc, char *argv[]){
     }
     
     //Stdin for second input...
-    if ((fd1 != 0 && bInput2) && argc < 4){
+    if ((fd1 != 0 && bInput2)){
         while((size = read(fd1, szBuf, sizeof szBuf)) > 0){
             if (wfd == -1){
                 write(STDOUT_FILENO,szBuf,size);
@@ -147,5 +147,5 @@ int main (int argc, char *argv[]){
     close(fd1);
     close(fd2);
     close(wfd);
-    return 0;
+    return 1;
 }
