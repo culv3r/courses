1f666656a4af1ce248acad8c810ae3cadb52a376
swf474 <swf474@fox04.cs.utsa.edu>
2016-11-07 16:09:37 -0600

    Update to nsim.c

--- cs/3423/assign7a/nsim.c
+++ cs/3423/assign7a/nsim.c
@@ -3,11 +3,33 @@
 #include <unistd.h>
 #include <sys/fcntl.h>
 #include <sys/types.h>
+#define PROCESS_IDS 8
 
 int main (int argc, char *argv[]){
-    int i;
-    while (i = 0; i < 8; i++){
-        fork();
+    int i,j,k;
+    pid_t pids[PROCESS_IDS+1];
+    int fd[PROCESS_IDS+1][2];
+    int iProcess;
+    for (k = 0; k<PROCESS_IDS; k++){
+        pipe(fd[k]);
+    }
+
+    for (i = 0; i < PROCESS_IDS; i++){
+        if ((pids[i] = fork()) < 0) {
+                perror("fork");
+                exit(1);
+            } else if (pids[i] == 0) {
+                iProcess = i;
+                for (j = 0; j < PROCESS_IDS; j++){
+                    if (j != iProcess){
+                        close(fd[j][0]);
+                    }else{
+                    close(fd[j][1]);
+                }
+            }   
+
+            exit(0);
+        }
     }
     return 0;
 }
